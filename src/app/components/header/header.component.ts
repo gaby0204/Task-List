242870c5f3626311e4ec69f4366c86f562f6de56
gabicyta0204 <gabicyta0204@gmail.com>
2022-09-17 10:10:18 -0300

    se agegra css del header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
    
-  title:string = 'Task-List';
+  title:string = 'My Task-List';
 
   constructor() { }
 
