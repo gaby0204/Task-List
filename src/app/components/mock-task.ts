3d68f1feab1532fa350c1c7d6c2f68e1075d23f2
gabicyta0204 <gabicyta0204@gmail.com>
2022-09-17 21:14:27 -0300

    Nueva lista de tareas

---
+++ src/app/components/mock-task.ts
@@ -0,0 +1,29 @@
+import {Task} from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Terminar primer modulo de Angular",
+        day: 'Septiembre 5 a las 13:00',
+        reminder: true,
+    },
+
+    {
+        id: 2,
+        text: "Hacer las compras para la cena",
+        day: 'Septiembre 5 a las 18:00',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: "Investigar sobre Boostrap",
+        day: 'Septiembre 5 a las 20:00',
+        reminder: false,
+    },
+    {
+        id: 4,
+        text: "Leer mi libro Favorito",
+        day: 'Septiembre 5 a las 22:00',
+        reminder: false,
+    },
+]
