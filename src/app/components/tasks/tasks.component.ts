411ae338d56469ee5ef4e239500457311d696860
gabicyta0204 <gabicyta0204@gmail.com>
2022-09-18 14:46:10 -0300

    servicio y servidor de tareas

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../service/task.service';
 import { Task } from '../Task';
-import { TASKS } from '../mock-task';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-tasks',
@@ -9,11 +9,15 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
  
-  constructor() { }
+  constructor( 
+    private taskService: TaskService
+  ) {}
 
   ngOnInit(): void {
-  }
+  this.taskService.getTasks().subscribe((tasks)=>(
+    this.tasks = tasks));
+     }
 
 }
