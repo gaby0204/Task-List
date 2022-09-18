0870a77b4d8862f41a4d8c20b14ffa79de84a23e
gabicyta0204 <gabicyta0204@gmail.com>
2022-09-17 22:31:48 -0300

    componente de lista de items

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from '../Task';
 import { TASKS } from '../mock-task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-tasks',
@@ -9,6 +10,7 @@ import { TASKS } from '../mock-task';
 })
 export class TasksComponent implements OnInit {
   tasks: Task[] = TASKS;
+ 
   constructor() { }
 
   ngOnInit(): void {
